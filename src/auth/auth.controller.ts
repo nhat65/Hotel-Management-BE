6df9f9617e4ab874a067530edd5a6735184072a9
Nhat <phantannhat12@gmail.com>
2025-09-09 16:22:11 +0700

    create register

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
+import { RegisterDto } from './dto/register.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -9,5 +10,10 @@ export class AuthController {
   @Post('login')
   login(@Body() loginDto: LoginDto, @Req() request) {
     return this.authService.login(loginDto, request);
+  }
+
+  @Post('register')
+  register(@Body() registerDto: RegisterDto) {
+    return this.authService.register(registerDto);
   }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,10 +5,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Account } from 'src/entities/account.entity';
 import { RefreshToken } from 'src/entities/refresh-token.entity';
 import { JwtModule } from '@nestjs/jwt';
+import { Staff } from 'src/entities/staff.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Account, RefreshToken]),
+    TypeOrmModule.forFeature([Account, RefreshToken, Staff]),
     JwtModule.register({}),
   ],
   controllers: [AuthController],

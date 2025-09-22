1667c9cacc3806f36e0b836bd93856db5105ac1f
Nhat <phantannhat12@gmail.com>
2025-09-22 15:01:30 +0700

    create update equipment

--- src/room-equipment/room-equipment.controller.ts
+++ src/room-equipment/room-equipment.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
 import { RoomEquipmentService } from './room-equipment.service';
 import { Roles } from 'src/common/decorators/role.decorator';
@@ -6,6 +6,7 @@ import { AccountRole } from 'src/entities/account.entity';
 import { RoomEquipmentDto } from './dto/room-equipment.dto';
 import { RolesGuard } from 'src/common/guards/role.guard';
 import { EquipmentStatus } from 'src/constants/enum';
+import { UpdateEquipmentDto } from './dto/update-equipment.dto';
 
 @Controller('room-equipment')
 @UseGuards(JwtAuthGuard)
@@ -24,5 +25,18 @@ export class RoomEquipmentController {
   @UseGuards(RolesGuard)
   async getEquipments(@Param('status') status: EquipmentStatus) {
     return await this.roomEquipmentService.getEquipments(status);
+  }
+
+  @Put('/:equipmentId/update')
+  @Roles(AccountRole.ADMIN)
+  @UseGuards(RolesGuard)
+  async update(
+    @Param('equipmentId') equipmentId: string,
+    @Body() updateEquipmentDto: UpdateEquipmentDto,
+  ) {
+    return await this.roomEquipmentService.update(
+      updateEquipmentDto,
+      equipmentId,
+    );
   }
 }
